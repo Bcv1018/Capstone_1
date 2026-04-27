20ce16025a5319e4e1e8b634c4d54ba463e92ccd
Bcv1018 <carrillobrandon98@gmail.com>
2026-04-27 14:56:50 -0700

    Added yearly comparison between South Region and Texas

--- Carrillo-Valencia_sales_analysis.sql
+++ Carrillo-Valencia_sales_analysis.sql
@@ -21,11 +21,32 @@ SELECT
 WHERE Store_ID BETWEEN 901 AND 911; --  Total Revenue = $3,417,850.01
 
 /*What is the month by month revenue breakdown for the sales territory?*/
+-- Month by Month Revenue from years 2022 to 2025
 SELECT 
- DATE_FORMAT(Transaction_Date, '%Y-%m') AS Monthly_Period
+ DATE_FORMAT(Transaction_Date, '%Y-%m') AS Monthly_Period -- formats the year and month in one column
 ,Format(SUM(Sale_Amount), 2) AS Monthly_Revenue FROM store_sales
 WHERE Store_ID BETWEEN 901 AND 911
 GROUP BY Monthly_Period
-ORDER BY Monthly_Period ASC; 
+ORDER BY Monthly_Period ASC; -- Orders monthly_revenue by the oldest date
 
-
+/*Provide a comparison of total revenue for the specific sales territory and the region it belongs to.*/
+-- Total revenue of the South Region comparing to the Total Revenue of Texas year by year
+SELECT
+	DATE_FORMAT(Transaction_Date, '%Y') AS `Year`
+    ,'Texas' AS Territory
+    ,Format(SUM(Sale_Amount), 2) AS Total_Revenue
+FROM store_sales
+WHERE Store_ID BETWEEN 901 AND 911
+GROUP BY Territory, `Year`
+UNION 
+SELECT
+	DATE_FORMAT(Transaction_Date, '%Y') AS `Year`
+    ,'South Region' AS Territory
+	,FORMAT(SUM(Sale_Amount),2) AS Total_Revenue
+FROM store_sales
+WHERE
+Store_ID BETWEEN 719 AND 729
+OR Store_Id BETWEEN 852 AND 853
+OR Store_Id BETWEEN 901 AND 911
+GROUP BY Territory, `Year`
+ORDER BY Year, Territory;
