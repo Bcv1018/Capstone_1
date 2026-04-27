b9b80eb62899ed6d3621df1b603669765c1116d4
Bcv1018 <carrillobrandon98@gmail.com>
2026-04-27 12:37:22 -0700

    Added Sales Analysis.sql file with Total revenue

---
+++ Carrillo-Valencia_sales_analysis.sql
@@ -0,0 +1,26 @@
+USE sample_sales;
+-- Finding Manager, region, state
+SELECT 
+	 SalesManager
+    ,Region
+    ,State 
+FROM management
+WHERE SalesManager LIKE '%Jeff "Howdy" Richards%'; -- Manger is Jeff "Howdy" Richards, Region is South, State is Texas
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+/*What is total revenue overall for sales in the assigned territory, plus the start date and end date
+that tell you what period the data covers?*/
+-- StoreLocation,Store ID = Arlington 901, Austin 902,Bacliff 903,Baytown 904,Beaumont 905, Cedar Park 906,Dallas 907,Denton 908,Desoto 909,Fort Worth F910,Georgetown 911
+SELECT * FROM store_locations
+WHERE State LIKE '%Texas';
+
+-- Total revenue overall 
+SELECT 
+ MIN(Transaction_Date) AS Start_Date
+,MAX(Transaction_Date) AS End_Date -- Time period from 2022-01-01 to 2025-12-31
+,Format(SUM(Sale_Amount), 2) AS Total_Revenue FROM store_sales
+WHERE Store_ID > 900; --  Total Revenue = $3,417,850.01
+
+/**/
+
+
+
