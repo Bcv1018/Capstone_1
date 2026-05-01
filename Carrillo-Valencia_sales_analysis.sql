1e82df031044644d7f3152ba2e3f96e2ae84522c
Bcv1018 <carrillobrandon98@gmail.com>
2026-04-30 22:23:15 -0700

    Added more comments to clarify what some of the queries are doing and added one more reccomendation

--- Carrillo-Valencia_sales_analysis.sql
+++ Carrillo-Valencia_sales_analysis.sql
@@ -31,6 +31,16 @@ ORDER BY Monthly_Period ASC; -- Orders monthly_revenue by the oldest date
 
 /*Provide a comparison of total revenue for the specific sales territory and the region it belongs to.*/
 -- Total revenue of the South Region comparing to the Total Revenue of Texas year by year
+-- Query to find what other states are in my region
+SELECT Region, State FROM management -- South Region states are Texas, FLorida, and South Carolina
+WHERE Region = 'South';
+-- Queries to find the storeIDs for Florida and South Carolina
+SELECT * FROM store_locations -- StoreIDs for Florida 719-729
+WHERE State = 'Florida';
+SELECT * FROM store_locations -- StoreID for South Carolina 852-853
+WHERE State = 'South Carolina';
+
+-- Query to compare Texas with the South Region yearly
 SELECT
 	DATE_FORMAT(Transaction_Date, '%Y') AS `Year`
     ,'Texas' AS Territory
@@ -55,8 +65,8 @@ ORDER BY Year, Territory;
 for the sales territory?*/
 SELECT
 	 DATE_FORMAT(Transaction_Date, '%Y-%m') AS `Date`
-    ,COUNT(Transaction_Date) AS Num_of_Transactions
-    ,FORMAT(AVG(Sale_Amount),2) AS AVG_Transaction_Size
+    ,COUNT(Transaction_Date) AS Num_of_Transactions -- Counts how many transaction there are
+    ,FORMAT(AVG(Sale_Amount),2) AS AVG_Transaction_Size -- Averages the transaction size by the sale amount
     ,i.Category
 FROM store_sales AS s
 JOIN products AS p ON s.Prod_Num = p.ProdNum
@@ -68,7 +78,7 @@ ORDER BY i.Category, `Date`;
 /*Can you provide a ranking of in-store sales performance by each store in the sales territory*/
 -- Tables store_sales and store_locations
 SELECT 
-	RANK() OVER (ORDER BY SUM(Sale_Amount) DESC) AS `Rank`
+	RANK() OVER (ORDER BY SUM(Sale_Amount) DESC) AS `Rank` -- Ranks the Sum of Sale_Amount from 1-11 1 being the highest performing and 11 lowest performing
     ,s.Store_ID
     ,l.StoreLocation
     ,FORMAT(SUM(Sale_Amount),2) AS Performance
@@ -80,4 +90,5 @@ ORDER BY `Rank`;
 
 /*What is your recommendation for where to focus sales attention in the next quarter?*/
  /* My recommendation based on rankings and performance is that for the next quarter we should focus on the Dallas location as it is the lowest performaing location in the Texas teritorry.
- With Dallas being the 9th most populous city in the US severly underperforming compared to the rest of the location is a problem and needs to be our highest priority.
+ With Dallas being the 9th most populous city in the US severly underperforming compared to the rest of the locations is a problem and needs to be our highest priority. I also reccomend to look at 
+ the stationery and supplies product category as it is one of our highest number of transaction but is our lowest performing products.
